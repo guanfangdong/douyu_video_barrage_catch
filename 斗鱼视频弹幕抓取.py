c69832eede00c1a28a636d8640da1d11db86b04b
guanfangdong <dong.guanfang.2014@gmail.com>
2019-06-07 12:42:08 -0600

    fix bug

--- 斗鱼视频弹幕抓取.py
+++ 斗鱼视频弹幕抓取.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import logging
 
 
+
 class DouYu:
     def __init__(self):
 
@@ -12,21 +13,22 @@ class DouYu:
         self.title = None
         self.file = None
         self.driver = None
-        self.barrage_dict = {}
+        self.barrage_list = []
         
         
 
     def get_barrage_info(self, barrage):
         all_barrage = barrage.split('\n')
+        print("正在截取第"+all_barrage[0]+"的弹幕\n")
         for i in range(len(all_barrage)):
             if i % 3 == 2:
                 try:
-                    self.barrage_dict[all_barrage[i - 2]] = all_barrage[i - 1]
+                    self.barrage_list.append(all_barrage[i - 2]+"&&&"+all_barrage[i - 1]+'\n')
                 except:
                     print("一条弹幕截取未成功")
 
     def run(self):
-        self.start_url =  input("请输入斗鱼视频的网址：")
+        self.start_url =  input("请输入斗鱼视频的网址：")        
         self.driver = webdriver.Chrome()
         self.driver.get(self.start_url)
         self.title = self.driver.find_element_by_class_name('video-title').text.split('\n')
@@ -49,9 +51,10 @@ class DouYu:
             actions.click()
             actions.perform()
         self.file = open(str(self.title[0]) + " 弹幕.txt", 'w')
-        for i in self.barrage_dict:
+        self.barrage_list = list(dict.fromkeys(self.barrage_list))
+        for i in self.barrage_list:
             try:
-                self.file.write(str(i)+"&&&"+str(self.barrage_dict[i])+"\n")
+                self.file.write(i)
             except:
                 print("一条弹幕截取未成功")
         print("完成")
