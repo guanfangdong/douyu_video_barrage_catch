5d0265e74c1e0965774bd3de86e31a84e02bdf94
guanfangdong <dong.guanfang.2014@gmail.com>
2019-06-07 07:50:00 -0600

    add programme

---
+++ 斗鱼视频弹幕抓取.py
@@ -0,0 +1,63 @@
+import json
+import time
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import logging
+
+
+class DouYu:
+    def __init__(self):
+
+        self.start_url = None
+        self.title = None
+        self.file = None
+        self.driver = None
+        self.barrage_dict = {}
+        
+        
+
+    def get_barrage_info(self, barrage):
+        all_barrage = barrage.split('\n')
+        for i in range(len(all_barrage)):
+            if i % 3 == 2:
+                try:
+                    self.barrage_dict[all_barrage[i - 2]] = all_barrage[i - 1]
+                except:
+                    print("一条弹幕截取未成功")
+
+    def run(self):
+        self.start_url =  input("请输入斗鱼视频的网址：")
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.start_url)
+        self.title = self.driver.find_element_by_class_name('video-title').text.split('\n')
+        barrage_element = self.driver.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='推荐视频'])[1]/following::div[1]")
+        barrage_element.click()
+        time.sleep(2)
+        actions = ActionChains(self.driver)
+        dragger = self.driver.find_element_by_id('mCSB_3_dragger_vertical')
+        actions.move_to_element_with_offset(dragger, 0, 30)
+        actions.perform() 
+        previous_barrageCont_text = None
+        barrageCont_text = None
+        while True:
+            barrageCont = self.driver.find_element_by_id('barrageCont')
+            barrageCont_text = barrageCont.text
+            if barrageCont_text == previous_barrageCont_text:
+                break
+            self.get_barrage_info(barrageCont_text)
+            previous_barrageCont_text = barrageCont_text
+            actions.click()
+            actions.perform()
+        self.file = open(str(self.title[0]) + " 弹幕.txt", 'w')
+        for i in self.barrage_dict:
+            try:
+                self.file.write(str(i)+"&&&"+str(self.barrage_dict[i])+"\n")
+            except:
+                print("一条弹幕截取未成功")
+        print("完成")
+        self.file.close()
+        self.driver.quit()
+
+if __name__=='__main__':
+    douyu = DouYu()
+    douyu.run()
